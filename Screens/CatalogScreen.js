6c8a3ec5bf448d6c94fed24640248bfeca5f8895
Tu Nombre <tu@example.com>
2024-10-02 12:07:34 -0500

    Actualización, muestra de productos

--- Screens/CatalogScreen.js
+++ Screens/CatalogScreen.js
@@ -1,7 +1,6 @@
 // screens/CatalogScreen.js
 import React from 'react';
-import { View, Dimensions } from 'react-native';
-//import Carousel from 'react-native-snap-carousel';
+import { View, Dimensions, FlatList } from 'react-native';
 import ProductCard from '../components/ProductCard';
 import globalStyles from '../styles/globalStyles';
 
@@ -23,13 +22,19 @@ const products = [
         id: '3', 
         name: 'Whisky', 
         price: 30, 
-        image: 'https://images.unsplash.com/photo-1555685813-96c2d527c2de?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDg4NzZ8MHwxfGFsbHwxfHx8fHx8fHwxNjE3MzM2NjY1&ixlib=rb-1.2.1&q=80&w=400' 
+        image: 'https://supermercadolaestacion.com/48463-large_default/whisky-jack-daniels-tradicional-botella-x-700-ml.jpg' 
     },
     { 
         id: '4', 
         name: 'Tequila', 
         price: 25, 
-        image: 'https://images.unsplash.com/photo-1517694712202-b8e7b70c27f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDg4NzZ8MHwxfGFsbHw0fHx8fHx8fHwxNjE3MzM2NjY1&ixlib=rb-1.2.1&q=80&w=400' 
+        image: 'https://licoresmedellin.com/cdn/shop/files/TEQUILA_DON_JULIO_70_BOTELL_700ml.jpg?v=1712781693&width=1024' 
+    },
+    { 
+        id: '5', 
+        name: 'Ron', 
+        price: 30, 
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvHPfXD4EftN4_P3XZTo9YkvHAPtioEKsY2A&s' 
     },
     // Agrega más productos según sea necesario
 ];
@@ -47,7 +52,14 @@ const CatalogScreen = ({ navigation }) => {
 
     return (
         <View style={globalStyles.container}>
-           
+            <FlatList
+                data={products}           // Datos que serán mostrados (productos)
+                renderItem={renderItem}    // Función que renderiza cada producto
+                keyExtractor={(item) => item.id} // Clave única para cada ítem
+                horizontal={true}         // Mostrar en horizontal
+                showsHorizontalScrollIndicator={false} // Ocultar la barra de desplazamiento horizontal
+                contentContainerStyle={{ paddingHorizontal: 10 }} // Agregar espaciado horizontal
+            />
         </View>
     );
 };
